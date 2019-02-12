a6b001c0dd2cd0277a2cb3a63aa562ce81e3f6b8
Artem Kashin <dev.tema@gmail.com>
2019-02-12 10:44:21 +0300

    Update Button Component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,14 +2,35 @@ import React from 'react';
 
 import s from './Button.module.scss';
 
+type Size = 'normal' | 'large';
+
 interface ButtonProps {
   title: string;
+  type: string;
+  size: Size;
+  onClick: () => void;
 }
 
-const Button = ({ title }: ButtonProps) => (
-  <button className={s.button}>
-    {title}
-  </button>
-);
+const Button = (props: ButtonProps) => {
+  let className = s.button;
+  if (props.size === 'large') {
+    className += ` ${s.button__large}`;
+  }
+  return (
+    <button
+      className={className}
+      type={props.type}
+      onClick={props.onClick}
+    >
+      {props.title}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  onClick: (): any => undefined,
+  size: 'normal',
+  type: 'button',
+};
 
 export default Button;
