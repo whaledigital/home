6d92cac96b5afad9a7be423269a67969fc71d430
Artem Kashin <dev.tema@gmail.com>
2019-02-12 10:43:59 +0300

    Add ThankYou Page

---
+++ src/pages/thank-you.tsx
@@ -0,0 +1,23 @@
+import { navigateTo } from 'gatsby';
+import React from 'react';
+
+import { withLayout } from 'components/layout/Layout';
+import Segment from 'components/segment/Segment';
+import Button from 'components/button/Button';
+import { SEO } from 'components/seo/SEO';
+
+const ThankYouPage = () => {
+  const navigateToHome = () => navigateTo('/');
+  return (
+    <>
+      <SEO title="Thank you" />
+      <Segment>
+        <h1>Thank you</h1>
+        <h2>for contacting us!</h2>
+        <Button title="Go to homepage" onClick={navigateToHome} />
+      </Segment>
+    </>
+  );
+};
+
+export default withLayout(ThankYouPage);
